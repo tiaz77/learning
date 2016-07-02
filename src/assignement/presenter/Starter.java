5db83104695c790a8c72d241d7edbfbd28832f9f
mattia <mattia.cattaneo@gmail.com>
2016-07-02 15:28:15 +0200

    aggiunto seminar (assignement 2)

--- src/assignement/presenter/Starter.java
+++ src/assignement/presenter/Starter.java
@@ -1,8 +1,15 @@
 package assignement.presenter;
 
+import java.io.FileNotFoundException;
+
 public class Starter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		if (args.length == 0) {
+			System.out.println("at least one candidate should be passed");
+			System.out.println("e.g. mattia dario stefano gennaro valentino vacca manlio enrico chicco ale matteo pino massi gabri");
+			return;
+		}
 		System.out.println(new Chooser(args).choose());
 	}
 
