7b01a92a9a52b726d4b2bc4e9113022e0c8e59ba
Mattia Cattaneo <mattia.cattaneo@gmail.com>
2016-12-04 18:33:29 +0100

    after refactoring introduceForeginMethod

--- src/assignement/fowler/refactoring/introduceforeignmethod/DateTest.java
+++ src/assignement/fowler/refactoring/introduceforeignmethod/DateTest.java
@@ -13,9 +13,13 @@ public class DateTest {
 	public void test() throws Exception {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 		Date previousEnd = dateFormat.parse("31.12.2016");
-		Date newStart = new Date (previousEnd.getYear(), previousEnd.getMonth(), previousEnd.getDate() + 1);
+		Date newStart = nextDay(previousEnd);
 		
 		assertEquals("01.01.2017", dateFormat.format(newStart));
+	}
+
+	private Date nextDay(Date previousEnd) {
+		return new Date (previousEnd.getYear(), previousEnd.getMonth(), previousEnd.getDate() + 1);
 	}
 
 }
